cbf4fce9f3868f91a75182883676b39510572b63
Ashleyasacenter <161303858+Ashleyasacenter@users.noreply.github.com>
2025-05-07 05:50:00 +0800

    Create DashboardController.php

---
+++ DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+class DashboardController {
+    public function getDashboardData() {
+        // Return dummy data or empty data structure
+        return [
+            'student_count' => 0,
+            'violation_count' => 0,
+            'pending_violations' => 0,
+            'violations' => []
+        ];
+    }
+}
+?>
